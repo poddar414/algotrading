3e58b821be77b8dd4b7b0a28f864d33ba3572ee9
Deepak Poddar <deepak.kumar.poddar@gmail.com>
2022-04-15 08:45:53 +0530

    my first  codewq
    Signed-off-by: Deepak Poddar <deepak.kumar.poddar@gmail.com>

---
+++ examples/my_apis.py
@@ -0,0 +1,364 @@
+import logging
+from kiteconnect import KiteConnect
+import time
+import json
+import datetime
+import numpy as np
+
+def my_init(debug_time,data_path='E:\\kite\\pykiteconnect\\history\\'):
+
+    trading_session = False
+    
+    # Check if login is required or not
+    system_time = time.localtime(time.time())
+    if (system_time.tm_hour >= 7) and trading_session == False and (system_time.tm_hour <= 16):
+    #if False:
+        kite = kite_login()
+        offset = -1
+        debug_session = False
+        holdings = kite.holdings()
+    else:
+        kite = 'NULL'
+        debug_session = True
+        holdings = [] 
+        with open(data_path+'NSE_NIFTY 50_.txt', 'r') as file:
+            json_data = file.read()
+            lines = json_data.split("\n")
+        file.close()
+        
+        yr_ref,mnth_ref,date_ref = debug_time.split()[0].split('-')
+        hr_ref,min_ref,sec_ref = debug_time.split()[1].split(':')
+
+        for offset, line in enumerate(lines):
+            json_acceptable_string = line.replace("'", "\"")
+            line = json.loads(json_acceptable_string)
+            timestamp = line['timestamp']
+            yr,mnth,date = timestamp.split()[0].split('-')
+            hr,min,sec = timestamp.split()[1].split(':')
+            if(((int(yr_ref) == int(yr)) and (int(mnth_ref) == int(mnth)) and (int(date_ref) == int(date))) and (int(hr) >= int(hr_ref)) and (int(min) >= int(min_ref)) and (int(sec) >= int(sec_ref))):
+                break
+            
+        offset = offset - len(lines)
+
+    watch_list = prep_watch_list('E:\\kite\\pykiteconnect\\watch_list.txt')
+    watch_list.append({'tradingsymbol':'NSE:NIFTY 50','weight':1.0,'last_price':0.0})
+    for holding in holdings:
+        stock_name = holding['tradingsymbol']
+        already_present = False
+        for watch in watch_list:
+            if watch['tradingsymbol'].split(':')[1] == stock_name:
+                already_present = True
+        if already_present == False:
+            watch_list.append({'tradingsymbol':'NSE:'+stock_name,'weight':1.0,'last_price':1.0})
+
+
+    watch_list_trading_symbol=[item['tradingsymbol'] for item in watch_list]
+    buy_trigger = np.zeros(len(watch_list_trading_symbol))
+    sell_trigger = np.zeros(len(watch_list_trading_symbol))
+
+    return kite, buy_trigger, sell_trigger, watch_list_trading_symbol, trading_session, debug_session,offset
+
+def kite_login():
+    my_api_key = '25v2r7e956zj2jk1'
+    my_api_secret= 'yxleie0cike2sai7dwm4459v72tqlylk'
+    kite = KiteConnect(api_key=my_api_key,debug=False)
+    print("Authenticate for sale")
+    print(kite.login_url())
+    print("Enter request token")
+    request_token = input()
+    data = kite.generate_session(request_token, my_api_secret)
+    kite.set_access_token(data["access_token"])
+    return kite
+
+def read_watch_list_from_file(file_name):
+    with open(file_name) as f:
+        lines = f.readlines()
+    f.close()
+    return lines
+
+def prep_watch_list(watch_list_file):
+    current_watch_list=[]
+
+    lines = read_watch_list_from_file(watch_list_file)
+    for line in lines:
+        line_split = line.split()
+        stock_name = line_split[0]
+        last_price = float(line_split[4])
+        weight = float(line_split[6])
+        entry = {'tradingsymbol':'NSE:'+stock_name,'weight':weight,'last_price':last_price}
+        
+        already_in_list = False
+        for item in current_watch_list:
+            if item['tradingsymbol'] == 'NSE:'+stock_name:
+                already_in_list = True
+
+        if already_in_list == False: 
+            current_watch_list.append(entry)
+
+    res_list = []
+    for i in range(len(current_watch_list)):
+        if current_watch_list[i] not in current_watch_list[i + 1:]:
+            res_list.append(current_watch_list[i])
+
+    return res_list
+
+def dump_last_prices(last_prices, watch_list, samples_per_decision, data_path='E:\\kite\\pykiteconnect\\history\\'):
+    for stock in watch_list:
+        with open(data_path+stock.replace(':',"_")+'_.txt', 'a') as file:
+            for line in last_prices[stock][samples_per_decision:]:
+                file.write(json.dumps(json.loads(line)))
+                file.write('\n')
+        file.close()
+
+def update_last_price(last_prices,kite, stock, trading_session, debug_session, offset,data_path='E:\\kite\\pykiteconnect\\history\\April-2022\\',my_logger=None):
+    
+    if trading_session == True:
+
+        try:
+            quote = kite.quote(stock)
+        except Exception as e:
+            got_ltp = False
+            my_logger.debug("First try of the {} quote failed \n".format(stock))
+            while got_ltp == False:
+                time.sleep(5)
+                try:
+                    quote = kite.quote(stock)
+                    got_ltp = True
+                except Exception as e2:
+                    my_logger.debug(" Not getting the {} last price \n".format(stock))
+
+
+        quote[stock]['timestamp'] = str(quote[stock]['timestamp'])
+        if(stock != 'NSE:NIFTY 50'):
+            quote[stock]['last_trade_time'] = str(quote[stock]['last_trade_time'])
+        last_prices[stock].append(json.dumps(quote[stock]))     # convert dictionary to string and then append       
+    elif debug_session == True:
+        with open(data_path+stock.replace(':',"_")+'_.txt', 'r') as file:
+            json_data = file.read()
+            lines = json_data.split("\n")
+            quote = lines[offset]
+        file.close()
+        last_prices[stock].append(quote)
+
+
+def sample_last_prices(last_prices,samples_per_decision=20,offset=-1):
+    
+    is_possible= False
+    cum_time_list = []
+    price_list =[]
+    timestamp = '0-0-0 0:0:0'
+    lines = last_prices
+
+    if samples_per_decision <= len(lines):
+        selected_lines = lines[-samples_per_decision:]
+        is_possible = True
+    else:
+        is_possible = False
+
+    time_list = []
+    price_list = []
+
+
+    if is_possible == True:  
+
+        consider_only_end_points = False
+
+        isEnmptyLinePresent = False
+        for line in selected_lines:
+            if line == '':
+                isEnmptyLinePresent = True
+
+        if isEnmptyLinePresent == True:        
+            selected_lines.remove('')
+
+        if consider_only_end_points == True:
+            selected_lines = [selected_lines[0], selected_lines[-1]] # correct problem because of new line
+
+        json_acceptable_string = selected_lines[0].replace("'", "\"")
+        line = json.loads(json_acceptable_string)
+
+        timestamp = line['timestamp']
+        last_price = line['last_price']
+        price_list.append(line['last_price'])
+        time_list.append(0)
+        prev_time_obj = time.strptime(timestamp.split()[-1], "%H:%M:%S")
+
+        for line in selected_lines[1:]:
+            if (line != '\n') and line != '':
+                json_acceptable_string = line.replace("'", "\"")
+                line = json.loads(json_acceptable_string)
+
+                timestamp = line['timestamp']
+                last_price = line['last_price']
+                price_list.append(last_price)
+                cur_time_obj = time.strptime(timestamp.split()[-1], "%H:%M:%S")
+                if cur_time_obj > prev_time_obj:
+                    time_list.append((cur_time_obj.tm_hour - prev_time_obj.tm_hour)*60*60 + 
+                    (cur_time_obj.tm_min - prev_time_obj.tm_min)*60+(cur_time_obj.tm_sec - prev_time_obj.tm_sec))
+                elif cur_time_obj < prev_time_obj:
+                    time_list.append(0.001)
+                else:
+                    time_list.append(0.001)
+                #    my_logger.debug('skipping the time stamp')
+                prev_time_obj = cur_time_obj
+
+        cum_time_list = [sum(time_list[0:x:1]) for x in range(1, len(time_list)+1)]   
+    return np.array(cum_time_list), np.array(price_list), timestamp, is_possible
+
+def prep_last_prices(watch_list,data_path='E:\\kite\\pykiteconnect\\history\\',samples_per_decision=50,offset=-1, my_logger=None):
+    
+    last_prices = {}
+    is_possible = False
+
+    for stock in watch_list:
+        if samples_per_decision != 0:
+            with open(data_path+stock.replace(':',"_")+'_.txt', 'r') as file:
+                json_data = file.read()
+                lines = json_data.split("\n")
+                if offset <= -1:
+                    if (samples_per_decision <=len(lines)):
+                    #if True:
+                        #selected_lines = lines[offset:offset+samples_per_decision]
+                        selected_lines = lines[-samples_per_decision:]
+                        is_possible = True
+                    else:
+                        is_possible = False
+                else:
+                    if samples_per_decision < len(lines):
+                        selected_lines = lines[-samples_per_decision:]
+                        is_possible = True
+                    else:
+                        is_possible = False
+                        my_logger.debug('could not read last prices of stock from file \n', stock)
+            file.close()
+
+        if is_possible == True:
+            last_prices[stock] = selected_lines
+        else:
+            last_prices[stock] = []
+            
+   
+    return last_prices
+
+def take_buy_decision(kite,slot_exec_flag,slot_exec_time,trading_session,watch_list_trading_symbol,system_time,slot_buy_amount,buy_trigger,timestamp_list,my_logger):
+
+    for idx, (flag,tic) in enumerate(zip(slot_exec_flag, slot_exec_time)):
+        tic_hr = (int)(tic.split(':')[0])
+        tic_min = (int)(tic.split(':')[1])
+        if (trading_session == True):
+            if (system_time.tm_hour >= tic_hr) and (system_time.tm_min >= tic_min) and flag == False:
+                slot_exec_flag[idx] = True
+
+                my_logger.debug("Maximum theta is {}".format(np.max(buy_trigger)))
+
+                buystock = watch_list_trading_symbol[np.where(buy_trigger == np.amax(buy_trigger))[0][0]].replace('NSE:','')
+                
+                if (buystock == 'NIFTY 50'):
+                    buystock = 'NIFTYBEES'
+
+
+                try:
+                    buy_quant = slot_buy_amount/kite.ltp('NSE:'+buystock)['NSE:'+buystock]['last_price']
+                    if buy_quant < 1.0:
+                        buy_quant = 1
+                    else:
+                        buy_quant = (int)(buy_quant)
+                    order_id = kite.place_order(variety=kite.VARIETY_REGULAR,
+                                    exchange=kite.EXCHANGE_NSE,
+                                    tradingsymbol=buystock,
+                                    transaction_type=kite.TRANSACTION_TYPE_BUY,
+                                    quantity=buy_quant, # buying quantatity should depend on current theta and momentum of theta
+                                    product=kite.PRODUCT_CNC,
+                                    order_type=kite.ORDER_TYPE_MARKET)
+                    my_logger.debug("Buy Order placed for stock {}".format(buystock))                                        
+                    print("Buy Order placed for stock {}".format(buystock))                                        
+                except Exception as e:                                        
+                    my_logger.debug("Buy Order could not be placed for stock {}".format(buystock))
+                    
+                buy_trigger = np.zeros(len(watch_list_trading_symbol)) #once will check this after removing
+                
+        else:
+            buy_indx = np.where(buy_trigger == np.amax(buy_trigger))[0][0]
+            if ((int(timestamp_list[buy_indx].split()[1].split(":")[0])) == tic_hr) and ((int(timestamp_list[buy_indx].split()[1].split(":")[1])) == tic_min) and flag == False:
+                buystock = watch_list_trading_symbol[np.where(buy_trigger == np.amax(buy_trigger))[0][0]].replace('NSE:','')
+                slot_exec_flag[idx] = True
+                buy_trigger = np.zeros(len(watch_list_trading_symbol))
+                my_logger.debug("Buy stock {} at time {}".format(buystock,timestamp_list[buy_indx]))
+                print("Buy stock {} at time {}".format(buystock,timestamp_list[buy_indx]))
+    return buy_trigger
+
+
+def take_sell_decision(kite,slot_exec_flag,slot_exec_time,trading_session,watch_list_trading_symbol,system_time,slot_sell_amount,sell_trigger,timestamp_list,my_logger):
+    for idx, (flag,tic) in enumerate(zip(slot_exec_flag, slot_exec_time)):
+        tic_hr = (int)(tic.split(':')[0])
+        tic_min = (int)(tic.split(':')[1])
+        if (trading_session == True):
+            if (system_time.tm_hour >= tic_hr) and (system_time.tm_min >= tic_min) and flag == False:
+                slot_exec_flag[idx] = True
+                exchange = ''
+                all_buy_index = np.where(sell_trigger >=0)
+                sell_trigger[all_buy_index] = 0
+                
+                while exchange == '':
+                    sell_index = np.where(sell_trigger == np.amin(sell_trigger))[0][0]
+                    sellstock = watch_list_trading_symbol[sell_index].replace('NSE:','')
+
+                    if (sellstock == 'NIFTY 50'):
+                        sellstock = 'NIFTYBEES'
+
+                    my_logger.debug("candidate sell stock is {}\n".format(sellstock))
+                    my_logger.debug("Minimum theta is {}".format(np.min(sell_trigger)))
+
+                    sell_trigger[sell_index] = 0
+                    quantity = 1
+
+                    for holding in kite.holdings():
+                        if holding['tradingsymbol'] == sellstock:
+                            exchange = holding['exchange']
+                            quantity = holding['quantity']
+                            my_logger.debug("Found the sell stock {} in holding \n".format(sellstock))
+                            break
+
+                    if np.sum(sell_trigger) == 0:
+                        break
+
+                if exchange == 'NSE':
+                    exchange = kite.EXCHANGE_NSE
+                elif exchange == 'BSE':
+                    exchange = kite.EXCHANGE_BSE
+                else:
+                    my_logger.debug("exchange is wrong. Could not find the sell stock in holdings at all \n")
+
+                if exchange != '':
+                    sell_quant = slot_sell_amount/kite.ltp('NSE:'+sellstock)['NSE:'+sellstock]['last_price']
+                    if sell_quant < 1.0:
+                        sell_quant = 1
+                    else:
+                        sell_quant = (int)(sell_quant)
+                    
+                    sell_quant = min(quantity,sell_quant)
+
+                    try: # need to check if stock is there in portfolio or not
+                        order_id = kite.place_order(variety=kite.VARIETY_REGULAR,
+                                        exchange=exchange,
+                                        tradingsymbol=sellstock,
+                                        transaction_type=kite.TRANSACTION_TYPE_SELL,
+                                        quantity=sell_quant,
+                                        product=kite.PRODUCT_CNC,
+                                        order_type=kite.ORDER_TYPE_MARKET)
+                        my_logger.debug("Sell Order placed for stock {} on exchange {} for quantaty {}".format(sellstock,exchange,sell_quant))                                            
+                    except Exception as e:                                        
+                        my_logger.debug("Sell Order could not be placed for stock {} on exchange {} for quantaty {}".format(sellstock,exchange,sell_quant))                                            
+                else:
+                    my_logger.debug('Exchange is wrong while placing the order')
+
+                sell_trigger = np.zeros(len(watch_list_trading_symbol)) #once will check this after removing
+        else:
+            sell_indx = np.where(sell_trigger == np.amin(sell_trigger))[0][0]
+            if ((int(timestamp_list[sell_indx].split()[1].split(":")[0])) == tic_hr) and flag == False:
+                sellstock = watch_list_trading_symbol[sell_indx].replace('NSE:','')
+                slot_exec_flag[idx] = True
+                sell_trigger = np.zeros(len(watch_list_trading_symbol))
+                my_logger.debug("Sell stock {} at time {}".format(sellstock,timestamp_list[sell_indx]))
+
+    return sell_trigger
--- examples/simple.py
+++ examples/simple.py
@@ -1,51 +1,105 @@
-import logging
-from kiteconnect import KiteConnect
+from my_apis import * 
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(filename="newfile.log",
+                    format='%(asctime)s %(message)s',
+                    filemode='w',
+                    level=logging.DEBUG)
 
-kite = KiteConnect(api_key="your_api_key")
+my_logger = logging.getLogger()
 
-# Redirect the user to the login url obtained
-# from kite.login_url(), and receive the request_token
-# from the registered redirect url after the login flow.
-# Once you have the request_token, obtain the access_token
-# as follows.
+## Main program starts here
+dump_live_quote = False
+get_last_price_from_local = True
+run_program = True
+scannin_speed = 0
+debug_print = False
+per_change_th = 2.0
+samples_per_decision=40
+debug_time = '2022-04-08 09:15:00'
+debug_iteration = 2000
 
-data = kite.generate_session("request_token_here", secret="your_secret")
-kite.set_access_token(data["access_token"])
+#slot_exec_buy_time = ['9:25', '9:40', '9:55', '10:10', '10:25', '10:40', '10:55', '11:10', '11:25', '12:00', '12:30', '13:00', '13:30', '14:00', '14:15', '14:30','14:45', '15:00', '15:14', '15:29']
+slot_exec_buy_time = ['9:40', '10:10', '10:40', '11:10', '12:00', '13:00', '14:00', '14:30', '15:00']
+slot_exec_buy_flag = [False for _ in slot_exec_buy_time]
 
-# Place an order
-try:
-    order_id = kite.place_order(
-        variety=kite.VARIETY_REGULAR,
-        exchange=kite.EXCHANGE_NSE,
-        tradingsymbol="INFY",
-        transaction_type=kite.TRANSACTION_TYPE_BUY,
-        quantity=1,
-        product=kite.PRODUCT_CNC,
-        order_type=kite.ORDER_TYPE_MARKET
-    )
+slot_exec_sell_time = ['15:00']
+slot_exec_sell_flag = [False for _ in slot_exec_sell_time]
 
-    logging.info("Order placed. ID is: {}".format(order_id))
-except Exception as e:
-    logging.info("Order placement failed: {}".format(e.message))
+buy_amount  = 20000
+sell_amount = 5000
+slot_buy_amount = buy_amount/len(slot_exec_buy_time)
+slot_sell_amount = sell_amount/len(slot_exec_sell_time)
 
-# Fetch all orders
-kite.orders()
+kite, buy_trigger, sell_trigger, watch_list_trading_symbol,trading_session,debug_session,offset = my_init(debug_time,data_path='E:\\kite\\pykiteconnect\\history\\April-2022\\')
+last_prices = prep_last_prices(watch_list_trading_symbol,data_path='E:\\kite\\pykiteconnect\\history\\April-2022\\',samples_per_decision=0,offset=offset,my_logger=my_logger)
 
-# Get instruments
-kite.instruments()
+while run_program == True:
+    
+    system_time = time.localtime(time.time())
+    #print(system_time)
+    # wait for market to open 
+    if (((system_time.tm_hour == 9) and (system_time.tm_min >= 15)) or (system_time.tm_hour > 9)) and kite != 'NULL':
+        trading_session = True
+    
+    # close the trading
+    if (((system_time.tm_hour == 15) and (system_time.tm_min >= 30)) or(system_time.tm_hour > 15)) and (trading_session == True):
+        trading_session = False
+        run_program = False
 
-# Place an mutual fund order
-kite.place_mf_order(
-    tradingsymbol="INF090I01239",
-    transaction_type=kite.TRANSACTION_TYPE_BUY,
-    amount=5000,
-    tag="mytag"
-)
+    if debug_session == True or trading_session == True:
+        theta_list =[]
+        stock_list = []
+        timestamp_list = []
+        for stock in watch_list_trading_symbol:
+            if debug_print == True:
+                print('************')
+                print('Current Price ::',kite.ltp(stock))
 
-# Cancel a mutual fund order
-kite.cancel_mf_order(order_id="order_id")
+            tics,price,timestamp, is_possible = sample_last_prices(last_prices[stock],samples_per_decision=samples_per_decision,offset=offset)
 
-# Get mutual fund instruments
-kite.mf_instruments()
+            if is_possible == True:
+                if tics.shape == price.shape:
+                    base_price = price[0]
+                    price = np.array([x/base_price  for x in price])
+                    theta = np.polyfit(tics,price,1)[0]
+                    
+                    theta_list.append(theta)
+                    stock_list.append(stock)
+                    timestamp_list.append(timestamp)
+                else:
+                    print('something went wrong')
+            else:
+                theta_list.append(0.0)
+                stock_list.append(stock)
+                timestamp_list.append('0-0-0 0:0:0')
+
+            update_last_price(last_prices,kite, stock, trading_session, debug_session,offset=offset + samples_per_decision,my_logger=my_logger)
+
+        if debug_session == True:
+            offset = offset + 1 # only used in debug mode
+            debug_iteration = debug_iteration - 1
+            if debug_iteration == 0:
+                run_program=False
+
+        sort_idx = sorted(range(len(theta_list)), key=lambda k: theta_list[k])
+        theta_list = [theta_list[i]*1000*1000 for i in sort_idx]
+        stock_list = [stock_list[i] for i in sort_idx]
+        timestamp_list = [timestamp_list[i] for i in sort_idx]
+
+        my_logger.debug("One New Set")
+
+        for stock,theta in zip(stock_list,theta_list):
+            my_logger.debug("{} theta is {}".format(stock,theta))
+            buy_trigger[watch_list_trading_symbol.index(stock)] = buy_trigger[watch_list_trading_symbol.index(stock)] + theta
+            sell_trigger[watch_list_trading_symbol.index(stock)] = sell_trigger[watch_list_trading_symbol.index(stock)] + theta
+
+        # Buy decision
+        buy_trigger = take_buy_decision(kite,slot_exec_buy_flag,slot_exec_buy_time,trading_session,watch_list_trading_symbol,system_time,slot_buy_amount,buy_trigger,timestamp_list,my_logger)
+
+        # Sell decision
+        sell_trigger = take_sell_decision(kite,slot_exec_sell_flag,slot_exec_sell_time,trading_session,watch_list_trading_symbol,system_time,slot_sell_amount,sell_trigger,timestamp_list,my_logger)
+
+        #time.sleep(1)
+
+if (debug_session == False) and (trading_session == True):
+    dump_last_prices(last_prices, watch_list_trading_symbol, samples_per_decision, data_path='E:\\kite\\pykiteconnect\\history\\April-2022\\')
